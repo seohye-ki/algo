d6e62edd30e4c014e64cfa23d4e0a9b82596bc6a
seohye-ki <seohyeon.shkim@gmail.com>
2024-08-16 02:52:28 +0900

    햄버거다이어트 풀이

--- swea/src/swea5215/Solution.java
+++ swea/src/swea5215/Solution.java
@@ -7,12 +7,9 @@ public class Solution {
 	static int L;
 	static int N;
 	static int M; //몇개까지
-	static int[] result;
-	static int[] kcal;
-	static int[] score;
-	static int[] visited;
 	static int max;
-	static int maxKcal;
+	static int ing[][];
+	
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -21,51 +18,41 @@ public class Solution {
 			max = 0;
 			N = sc.nextInt(); //입력받을 음식 수
 			L = sc.nextInt(); //최대 칼로리
-			kcal = new int[N]; //칼로리 배열
-			score = new int[N]; //맛점수 배열
+			ing = new int[N][2];
 			
 			for(int i = 0 ; i < N; i++) {
-				score[i] = sc.nextInt();
-				kcal[i] = sc.nextInt();
+				
+				ing[i][0] = sc.nextInt(); //점수 
+				ing[i][1] = sc.nextInt(); //칼로리 
 			}
+			Arrays.sort(ing, (a, b)->(a[1] - b[1]));
+//			System.out.println(Arrays.deepToString(ing));
 			
 			for(int i = 0; i < N; i++) {
 				M = i + 1;
-				result = new int[M];
-				visited = new int[N];
-				maxKcal = 0;
-				dfs(0, 0);
+				dfs(0, 0, 0, 0);
 			}
+			
 			System.out.println("#" + test_case + " " + max);
 		}		
 		sc.close();
 	}
 	
-	static void dfs(int start, int cnt) {
+	static void dfs(int start, int cnt, int sumScore, int sumKcal) {
+		if(sumKcal > L)
+			return ;
+		
 		if(cnt == M) {
-			int sumKcal = 0;
-			int sumScore = 0;
-			for(int i = 0; i < M; i++) {
-				sumKcal += kcal[result[i]];
-				sumScore += score[result[i]];
-			}
-			
+//			System.out.println("===============");
+//			System.out.println("sums : " + sumScore + " sumk : " + sumKcal);
 			if(sumKcal <= L && sumScore > max) {
-				
-				System.out.println(Arrays.toString(result));
-				System.out.println("sumk : " + sumKcal + " sums : " + sumScore);
 				max = sumScore;
 			}
 			return ;
 		}
-		
-		for(int i = 0; i < N; i++) {
-			if(visited[i] == 0) {
-				visited[i] = 1;
-				result[cnt] = i;
-				dfs(i + 1, cnt + 1);
-				visited[i] = 0;
-			}
+
+		for(int i = start; i < N; i++) {
+			dfs(i + 1, cnt + 1, sumScore + ing[i][0], sumKcal + ing[i][1]);
 		}
 	}
 }
