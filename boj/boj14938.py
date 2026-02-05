2716a9fd32d7c6f597f0fdc1ecea61a251653f14
seohye-ki <94886139+seohye-ki@users.noreply.github.com>
2026-02-05 13:07:51 +0900

    260205 boj14938

---
+++ boj/boj14938.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+items = [0] + list(map(int, input().split()))
+
+INF = float('inf')
+dist = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    dist[i][i] = 0
+
+for _ in range(r):
+    a, b, l = map(int, input().split())
+    dist[a][b] = min(dist[a][b], l)
+    dist[b][a] = min(dist[b][a], l)
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+max_items = 0
+for i in range(1, n + 1):
+    total = 0
+    for j in range(1, n + 1):
+        if dist[i][j] <= m:
+            total += items[j]
+    max_items = max(max_items, total)
+
+print(max_items)
