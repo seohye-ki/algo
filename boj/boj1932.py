ad9a7e2a7ccc8e0351df2b0c05f0477ece549514
seohye-ki <94886139+seohye-ki@users.noreply.github.com>
2026-03-05 22:22:01 +0900

    260305 boj1932

---
+++ boj/boj1932.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+triangle = []
+for _ in range(n):
+    triangle.append(list(map(int, input().split())))
+
+for i in range(n-2, -1, -1):
+    for j in range(i+1):
+        triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1])
+
+print(triangle[0][0])
