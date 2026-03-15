ec55634288db552dcd5ff073c31316579664a4c0
seohye-ki <94886139+seohye-ki@users.noreply.github.com>
2026-03-15 21:06:53 +0900

    260315 boj17298

---
+++ boj/boj17298.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = [-1] * n
+stack = []
+
+for i in range(n):
+    while stack and arr[stack[-1]] < arr[i]:
+        result[stack.pop()] = arr[i]
+    stack.append(i)
+
+print(*result)
